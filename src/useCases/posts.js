ba7e3c0376322227d1cc5a8b3e2841d431de7fb2
RubS <rub.somuano.g@gmail.com>
2022-03-04 02:28:04 -0600

    featurePost

--- src/useCases/posts.js
+++ src/useCases/posts.js
@@ -3,5 +3,24 @@ const Post = require("../models/posts");
 function getAllPost() {
   return Post.find({});
 }
+function createPost(dataPost) {
+  const { title, tags, content, writer, date } = dataPost;
+  return Post.create({ title, tags, content, writer, date });
+}
+function getPostById(idPost) {
+  return Post.findById(idPost);
+}
+function patchPostById(idPost, dataToUpdate) {
+  return Post.findByIdAndUpdate(idPost, dataToUpdate, { new: true });
+}
+function deletePostById(idPost) {
+  return Post.findById(idPost);
+}
 
-module.exports = { getAllPost };
+module.exports = {
+  getAllPost,
+  createPost,
+  getPostById,
+  patchPostById,
+  deletePostById,
+};
