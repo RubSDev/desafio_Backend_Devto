04f09b626cb6acd60d04104f3eee72434ec393f9
DesignConvertia <designconvertia@DesignConvertias-MacBook-Air.local>
2022-03-05 12:31:34 -0600

    Merge and fix models

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,16 +1,6 @@
 const mongoose = require("mongoose");
 
-//const {DB_USER, DB_PASSWORD, DB_HOST, DB_NAME} = process.env
-
-/*const DB_USER = "desafioBackend";
-const DB_PASSWORD = "jesrobrub";
-const DB_HOST = "cluster0.kg3lw.mongodb.net";
-const DB_NAME = "devto";*/
-
-const DB_USER = process.env.DB_USER
-const DB_PASSWORD = process.env.DB_PASSWORD
-const DB_HOST = process.env.DB_HOST
-const DB_NAME = process.env.DB_NAME
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 const URL = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`;
 
