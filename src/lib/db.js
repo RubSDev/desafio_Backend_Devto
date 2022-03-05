4514f49843e0feb0ae866aa7400f2f2655087d60
DesignConvertia <designconvertia@DesignConvertias-MacBook-Air.local>
2022-03-05 12:36:24 -0600

    Delete credentials

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,9 +1,5 @@
 const mongoose = require("mongoose");
 
-const DB_USER = "desafioBackend";
-const DB_PASSWORD = "jesrobrub";
-const DB_HOST = "cluster0.kg3lw.mongodb.net";
-const DB_NAME = "devto";
 const URL = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`;
 
 function connect() {
